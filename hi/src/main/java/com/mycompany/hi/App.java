64f1c69632e10643d6c3346e179832721ae8fe01
brandon story <brandonmichaelstory@gail.com>
2019-08-29 11:17:56 -0600

    Created class

---
+++ hi/src/main/java/com/mycompany/hi/App.java
@@ -0,0 +1,24 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.mycompany.hi;
+
+/**
+ *
+ * @author micha
+ */
+public class App {
+    
+    public static void main(String[] args) throws Exception {
+        
+        App app = new App();
+        app.run();
+    }
+
+    private void run() {
+       
+        System.out.println("Hello World");
+    }
+}
