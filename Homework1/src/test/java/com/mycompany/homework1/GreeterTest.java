a5f40740eaf23f874a9110c6ae59b587b1f707ee
brandon story <brandonmichaelstory@gail.com>
2019-09-09 19:37:09 -0600

    added more tests

--- Homework1/src/test/java/com/mycompany/homework1/GreeterTest.java
+++ Homework1/src/test/java/com/mycompany/homework1/GreeterTest.java
@@ -27,17 +27,27 @@ public class GreeterTest {
         assertEquals(expResult, result);
     }
 
+    @Test
+    public void testGetGreetingWithCurrent() {
+        //System.out.println("getGreeting");
+        Greeter instance = new Greeter();
+        String expResult = "Howdy!";
+        instance.setGreeting(expResult);
+        String result = instance.getGreeting();
+        assertEquals(expResult, result);
+    }
     /**
      * Test of setGreeting method, of class Greeter.
      */
     @Test
     public void testSetGreeting() {
-        System.out.println("setGreeting");
-        String greeting = "";
+        //System.out.println("setGreeting");
+        String greeting = "Hey There!";
         Greeter instance = new Greeter();
         instance.setGreeting(greeting);
+        String expResult = instance.getGreeting();
+        assertEquals(expResult, greeting);
         // TODO review the generated test code and remove the default call to fail.
-        fail("The test case is a prototype.");
     }
     
 }
