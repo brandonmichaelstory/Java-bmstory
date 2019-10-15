4c081bd6e3e13c026cf2f5d748aecd20102d9e02
brandon story <brandonmichaelstory@gail.com>
2019-10-15 16:58:22 -0600

    created Food class and added cleanInput function and addFood function

--- CollectionHomework/src/main/java/com/mycompany/collectionhomework/FoodMap.java
+++ CollectionHomework/src/main/java/com/mycompany/collectionhomework/FoodMap.java
@@ -4,14 +4,39 @@
  * and open the template in the editor.
  */
 package com.mycompany.collectionhomework;
-
+import java.util.Scanner;
+import java.util.HashMap;
 /**
  *
  * @author micha
  */
+
+class Food {
+    
+    int calories, protein, carbs, fat;
+    public Food(int calories, int protein, int carbs, int fat) {
+        this.calories = calories;
+        this.protein = protein;
+        this.carbs = carbs;
+        this.fat = fat;
+    }
+    public Food(int calories) {
+        this.calories = calories;
+    }
+    public int getCalories() { return this.calories; }
+    public int getProtein() { return this.protein; }
+    public int getCarbs() { return this.carbs; }
+    public int getFat() { return this.fat; }
+    public void setCalories(int calories) { this.calories = calories; }
+    public void setProtein(int protein) { this.protein = protein; }
+    public void setCarbs(int carbs) { this.carbs = carbs; }
+    public void setFat(int fat) { this.fat = fat; } 
+}
+
 public class FoodMap {
     
-    
+    Scanner forConsole = new Scanner(System.in);
+    HashMap<String, Food> foods = new HashMap<>();
     public static void main(String[] args) {
         FoodMap fm = new FoodMap();
         fm.recieveInput();
@@ -20,6 +45,8 @@ public class FoodMap {
         
         boolean continueInput = true;
         String answer;
+        answer = forConsole.nextLine();
+        answer = cleanInput(answer);
         System.out.println("What would you like to do?");
         System.out.println("Add a new food");
         System.out.println("Edit an old food");
@@ -38,5 +65,32 @@ public class FoodMap {
             continueInput = false;
         }
         return continueInput;
+    }
+    
+     public String cleanInput(String answer) {
+        
+        answer = answer.toLowerCase();
+        answer = answer.stripLeading();
+        answer = answer.stripTrailing();
+        return answer;
+    }
+     
+    public void addFood() {
+        System.out.println("<addFood>");
+        String name;
+        int calories, protein, carbs, fat;
+        System.out.println("Enter the name of the food, calories, protein, carbs, fat.");
+        name = forConsole.nextLine();
+        name = cleanInput(name);
+        if (!foods.containsKey(name)) {
+            calories = forConsole.nextInt();
+            protein = forConsole.nextInt();
+            carbs = forConsole.nextInt();
+            fat = forConsole.nextInt();
+            forConsole.nextLine();
+            Food f = new Food(calories, protein, carbs, fat);
+            this.foods.put(name, f);
+        }
+        
     }
 }
