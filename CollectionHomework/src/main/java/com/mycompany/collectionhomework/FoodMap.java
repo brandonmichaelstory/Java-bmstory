3fdfe78c767f3ce9afb25425a96b1819dc244095
brandon story <brandonmichaelstory@gail.com>
2019-10-15 16:33:40 -0600

    created recieveInput Function

--- CollectionHomework/src/main/java/com/mycompany/collectionhomework/FoodMap.java
+++ CollectionHomework/src/main/java/com/mycompany/collectionhomework/FoodMap.java
@@ -11,4 +11,32 @@ package com.mycompany.collectionhomework;
  */
 public class FoodMap {
     
+    
+    public static void main(String[] args) {
+        FoodMap fm = new FoodMap();
+        fm.recieveInput();
+    }
+    public boolean recieveInput()  {
+        
+        boolean continueInput = true;
+        String answer;
+        System.out.println("What would you like to do?");
+        System.out.println("Add a new food");
+        System.out.println("Edit an old food");
+        System.out.println("View the info about a food");
+        System.out.println("exit");
+        if (answer.equals("add")) {
+            addFood();
+        }
+        else if (answer.equals("edit")) {
+            editFood();
+        }
+        else if (answer.equals("view")) {
+            viewFood();
+        }
+        else if (answer.equals("exit")) {
+            continueInput = false;
+        }
+        return continueInput;
+    }
 }
