6617e1c24c3ea17968ce568bf19309da4ad0e6e8
brandon story <brandonmichaelstory@gail.com>
2019-10-30 19:32:42 -0600

    added conect function

--- DBHomework/src/main/java/com/mycompany/dbhomework/AlbumsDatabase.java
+++ DBHomework/src/main/java/com/mycompany/dbhomework/AlbumsDatabase.java
@@ -5,10 +5,26 @@
  */
 package com.mycompany.dbhomework;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
 /**
  *
  * @author micha
  */
-public class App {
+public class AlbumsDatabase {
     
+    public static String DEFAULT_DB = "testAlbums.db";
+    public static String DEFAULT_URL = "jdbc:sqlite:" + DEFAULT_DB;
+
+    private Connection connect() {
+        Connection conn = null;
+        try {
+            conn = DriverManager.getConnection(this.DEFAULT_URL);
+        } catch (SQLException e) {
+            System.out.println(e.getMessage());
+        }
+        return conn;
+    }
 }
