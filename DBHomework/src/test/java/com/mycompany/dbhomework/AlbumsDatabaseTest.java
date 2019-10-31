8fb1582e04edb527d8a60814505f0ea2078fab55
brandon story <brandonmichaelstory@gail.com>
2019-10-30 23:45:10 -0600

    updated createNewDatabaseTest

---
+++ DBHomework/src/test/java/com/mycompany/dbhomework/AlbumsDatabaseTest.java
@@ -0,0 +1,86 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.mycompany.dbhomework;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import org.junit.*;
+import static org.junit.Assert.*;
+
+/**
+ *
+ * @author micha
+ */
+public class AlbumsDatabaseTest {
+    
+    public AlbumsDatabaseTest() {
+    }
+    
+
+    /**
+     * Test of createNewDatabase method, of class AlbumsDatabase.
+     */
+    @Test
+    public void testCreateNewDatabase() {
+        System.out.println("createNewDatabase");
+        AlbumsDatabase instance = new AlbumsDatabase();
+        instance.createNewDatabase();
+        try (Connection connection = DriverManager.getConnection(instance.DEFAULT_URL)) {
+            ResultSet resultSet = connection.getMetaData().getCatalogs();
+            resultSet.next();
+            String dbName = resultSet.getString(1);
+            System.out.println(dbName);
+            assertEquals("test.db", dbName);
+        }
+        catch (SQLException e) {
+            System.out.println(e.toString());
+        }
+    }
+
+    /**
+     * Test of createNewTable method, of class AlbumsDatabase.
+     */
+    @Test
+    public void testCreateNewTable() {
+        System.out.println("createNewTable");
+        String tableName = "";
+        AlbumsDatabase instance = new AlbumsDatabase();
+        instance.createNewTable(tableName);
+        // TODO review the generated test code and remove the default call to fail.
+        fail("The test case is a prototype.");
+    }
+
+    /**
+     * Test of deleteTable method, of class AlbumsDatabase.
+     */
+    @Test
+    public void testDeleteTable() {
+        System.out.println("deleteTable");
+        String tableName = "";
+        AlbumsDatabase instance = new AlbumsDatabase();
+        instance.deleteTable(tableName);
+        // TODO review the generated test code and remove the default call to fail.
+        fail("The test case is a prototype.");
+    }
+
+    /**
+     * Test of insert method, of class AlbumsDatabase.
+     */
+    @Test
+    public void testInsert() {
+        System.out.println("insert");
+        String tableName = "";
+        String AlbumName = "";
+        int year = 0;
+        AlbumsDatabase instance = new AlbumsDatabase();
+        instance.insert(tableName, AlbumName, year);
+        // TODO review the generated test code and remove the default call to fail.
+        fail("The test case is a prototype.");
+    }
+    
+}
