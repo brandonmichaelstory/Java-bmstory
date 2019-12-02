c69f11e54cad36fbcd137b9ba21b1fc885fdb139
brandon story <brandonmichaelstory@gail.com>
2019-12-02 12:49:07 -0700

    added Deck class and Suit enum

--- FinalProject/src/main/java/com/mycompany/finalproject/Game.java
+++ FinalProject/src/main/java/com/mycompany/finalproject/Game.java
@@ -5,18 +5,28 @@
  */
 package com.mycompany.finalproject;
 
+import java.util.ArrayList;
+import java.util.Random;
+
 /**
  *
  * @author micha
  */
+
+enum Suit {
+    DIAMONDS, HEARTS, CLUBS, SPADES;
+}
+
 class Card {
     
     private int value; //card integer value
     private String cardName;
-    public Card(int cardIndex) {
+    private Suit suit;
+    public Card(int cardIndex, Suit suit) {
         
         this.value = setValue(cardIndex); // card index being the number to represent the card ex. Ace index = 1
         this.cardName = getName(cardIndex);
+        this.suit = suit;
     }
     
     public int getValue() {
@@ -49,12 +59,60 @@ class Card {
         }
     }
     
+    @Override
+    public String toString() {
+        
+        StringBuilder s = new StringBuilder();
+        s.append(this.cardName);
+        switch (suit) {
+            case DIAMONDS:
+                s.append("D");
+                break;
+            case CLUBS:
+                s.append("C");
+                break;
+            case HEARTS:
+                s.append("H");
+                break;
+            case SPADES:
+                s.append("S");
+                break;
+            }
+        return s.toString();
+    }
+    
+}
+
+class Deck {
+    
+    Random rand = new Random();
+    private ArrayList<Card> deck = new ArrayList<>();
+    public Deck() {
+    
+        buildDeck();
+    }
+    
+    private void buildDeck() {
+        
+        for (Suit s : Suit.values()) {
+            
+            for (int i = 1; i <= 13; i++) {
+                deck.add(new Card(i, s));
+            }
+        }
+    }
+    
+    public Card hit() {
+        
+        int index = rand.nextInt(deck.size());
+        Card c = deck.get(index);
+        deck.remove(index);
+        return c;
+    }
 }
 public class Game {
     
     public static void main(String[] args) {
         
-        Card c = new Card(11);
-        System.out.println(Integer.toString(c.getValue()) + c.getName(11));
     }
 }
