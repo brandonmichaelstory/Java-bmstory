7cdf67ca8d51609dcf4560c33ffb3211a5da3e7b
brandon story <brandonmichaelstory@gail.com>
2019-12-02 12:42:07 -0700

    created Card class

--- FinalProject/src/main/java/com/mycompany/finalproject/Game.java
+++ FinalProject/src/main/java/com/mycompany/finalproject/Game.java
@@ -9,6 +9,52 @@ package com.mycompany.finalproject;
  *
  * @author micha
  */
+class Card {
+    
+    private int value; //card integer value
+    private String cardName;
+    public Card(int cardIndex) {
+        
+        this.value = setValue(cardIndex); // card index being the number to represent the card ex. Ace index = 1
+        this.cardName = getName(cardIndex);
+    }
+    
+    public int getValue() {
+        return value;
+    }
+    
+    public String getName(int cardIndex) {
+        
+        switch (cardIndex) {
+            case 1:
+                return "A";
+            case 11:
+                return "J";
+            case 12:
+                return "Q";
+            case 13:
+                return "K";
+            default:
+                return Integer.toString(cardIndex);
+        }
+    }
+    
+    public int setValue(int cardIndex) {
+        
+        if (cardIndex <= 9) {
+            return cardIndex;
+        }
+        else {
+            return 10;
+        }
+    }
+    
+}
 public class Game {
     
+    public static void main(String[] args) {
+        
+        Card c = new Card(11);
+        System.out.println(Integer.toString(c.getValue()) + c.getName(11));
+    }
 }
