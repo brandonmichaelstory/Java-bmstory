9a2476122cef00917774e7ca57a6517e612592d4
brandon story <brandonmichaelstory@gail.com>
2019-12-04 11:22:21 -0700

    updated tests

--- FinalProject/src/test/java/com/mycompany/finalproject/CardTest.java
+++ FinalProject/src/test/java/com/mycompany/finalproject/CardTest.java
@@ -12,6 +12,8 @@ import static org.junit.Assert.*;
  *
  * @author micha
  */
+
+///make sure to fix the test because it fails on the toString because all cards are considered hearts
 public class CardTest {
     
     public CardTest() {
@@ -23,27 +25,26 @@ public class CardTest {
     @Test
     public void testGetValue() {
         System.out.println("getValue");
-        Card instance = null;
-        int expResult = 0;
-        int result = instance.getValue();
-        assertEquals(expResult, result);
-        // TODO review the generated test code and remove the default call to fail.
-        fail("The test case is a prototype.");
+        Card instance1 = new Card(1, Suit.DIAMONDS);
+        Card instance2 = new Card(3, Suit.CLUBS);
+        int expResult1 = 11;
+        int expResult2 = 3;
+        assertEquals(expResult1, instance1.getValue());
+        assertEquals(expResult2, instance2.getValue());
     }
 
     /**
      * Test of getName method, of class Card.
      */
     @Test
-    public void testGetName() {
+    public void testCreateName() {
         System.out.println("getName");
-        int cardIndex = 0;
-        Card instance = null;
-        String expResult = "";
-        String result = instance.getName(cardIndex);
-        assertEquals(expResult, result);
-        // TODO review the generated test code and remove the default call to fail.
-        fail("The test case is a prototype.");
+        Card instance1 = new Card(1, Suit.DIAMONDS);
+        Card instance2 = new Card(3, Suit.CLUBS);
+        String expResult1 = "A";
+        String expResult2 = "3";
+        assertEquals(expResult1, instance1.getCardName());
+        assertEquals(expResult2, instance2.getCardName());
     }
 
     /**
@@ -52,13 +53,13 @@ public class CardTest {
     @Test
     public void testSetValue() {
         System.out.println("setValue");
-        int cardIndex = 0;
-        Card instance = null;
-        int expResult = 0;
-        int result = instance.setValue(cardIndex);
-        assertEquals(expResult, result);
-        // TODO review the generated test code and remove the default call to fail.
-        fail("The test case is a prototype.");
+        Card instance1 = new Card(1, Suit.DIAMONDS);
+        Card instance2 = new Card(3, Suit.CLUBS);
+        int expResult1 = 11;
+        int expResult2 = 3;
+        
+        assertEquals(expResult1, instance1.getValue());
+        assertEquals(expResult2, instance2.getValue());
     }
 
     /**
@@ -66,13 +67,13 @@ public class CardTest {
      */
     @Test
     public void testGetCardName() {
-        System.out.println("getCardName");
-        Card instance = null;
-        String expResult = "";
-        String result = instance.getCardName();
-        assertEquals(expResult, result);
-        // TODO review the generated test code and remove the default call to fail.
-        fail("The test case is a prototype.");
+        
+        Card instance1 = new Card(1, Suit.DIAMONDS);
+        Card instance2 = new Card(3, Suit.CLUBS);
+        String expResult1 = "A";
+        String expResult2 = "3";
+        assertEquals(expResult1, instance1.getCardName());
+        assertEquals(expResult2, instance2.getCardName());
     }
 
     /**
@@ -81,12 +82,10 @@ public class CardTest {
     @Test
     public void testToString() {
         System.out.println("toString");
-        Card instance = null;
-        String expResult = "";
-        String result = instance.toString();
-        assertEquals(expResult, result);
-        // TODO review the generated test code and remove the default call to fail.
-        fail("The test case is a prototype.");
+        Card instance1 = new Card(1, Suit.DIAMONDS);
+        Card instance2 = new Card(3, Suit.CLUBS);
+        assertEquals("AD", instance1.toString());
+        assertEquals("3C", instance2.toString());
     }
     
 }
